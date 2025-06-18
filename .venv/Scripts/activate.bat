5d40a2a981d262875d06d86490f55e01cc795567
marcely melo <marcelyjfmelo@gmail.com>
2025-06-18 09:16:37 -0300

    atualização do gitignore

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Marcely\Desktop\PISI3_grupo05_2025.1\.venv
+set VIRTUAL_ENV=C:\Users\Marcely\Desktop\teste\PISI3_grupo05_2025.1\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
